12b23a693dccd7c49f98b0dbadd91ee6c1ec7822
Omar Issa Attar <oi156f@att.com>
2018-05-24 16:11:28 -0700

    added loading indicator

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.example.androidjokes.JokeActivity;
 import com.example.jokesmith.Joker;
@@ -14,11 +16,15 @@ import com.example.jokesmith.Joker;
 public class MainActivity extends AppCompatActivity implements JokeListener {
 
     Joker joker;
+    ProgressBar progressBar;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progressBar);
+        button = findViewById(R.id.button);
         joker = new Joker();
     }
 
@@ -46,11 +52,15 @@ public class MainActivity extends AppCompatActivity implements JokeListener {
     }
 
     public void tellJoke(View view) {
+        button.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
         new JokeFetcher().execute(this);
     }
 
     @Override
     public void onJokeLoaded(String joke) {
+        progressBar.setVisibility(View.GONE);
+        button.setVisibility(View.VISIBLE);
         Intent intent = new Intent(this, JokeActivity.class);
         intent.putExtra(Intent.EXTRA_TEXT, joker.getJoke());
         startActivity(intent);
