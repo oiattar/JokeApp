8866f32067e8a4be7656311001dd366d44e31ffa
oiattar <oiattar1@gmail.com>
2018-05-27 12:59:46 -0700

    moved url to gradle

--- app/src/main/java/com/udacity/gradle/builditbigger/JokeFetcher.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/JokeFetcher.java
@@ -24,7 +24,7 @@ public class JokeFetcher extends AsyncTask<JokeListener, Void, String> {
         if (apiService == null) {
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setRootUrl(BuildConfig.URL)
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> request) throws IOException {
